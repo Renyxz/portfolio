9153a29554f1db7caf4a3871911ff6d140b6b8e8
Ren <ren.yxz.dev@gmail.com>
2017-10-19 18:11:39 -0400

    Implemented routing for future use

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './css/index.css';
 import './css/App.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<Switch>
+			<Route path="/" component={App} />
+		</Switch>
+	</BrowserRouter>
+	, document.getElementById('root'));
 registerServiceWorker();
