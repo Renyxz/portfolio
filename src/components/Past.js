9037e5806010d35aec6ba771c20ac175f528cfa3
Ren <ren.yxz.dev@gmail.com>
2017-10-21 16:36:25 -0400

    Created new component for past projects page

---
+++ src/components/Past.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Past extends Component {
+	render() {
+		return(
+				<div className="row">
+					Past Projects page
+				</div>
+			);
+	}
+}
+
+export default Past;
