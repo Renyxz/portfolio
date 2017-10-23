e13d575f25b0bbc789609aa011f4c24fd9d2bd24
Ren <ren.yxz.dev@gmail.com>
2017-10-22 22:09:16 -0400

    Added buttons for create new post and edit posts, routes has been implemented for both

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Dashboard extends Component {
 	render() {
 		return(
-				<div className="row">
-					This is the dashboard
+				<div className="page-container row">
+					<div className="dash-buttons-container">
+						<Link to="/dashboard/create-post">
+							+ Make a new post
+						</Link>
+
+						<Link to="/dashboard/posts">
+							Edit posts
+						</Link>
+					</div>
 				</div>
 			);
 	}
